7b27da3b403101b0ae969d93c9dc6c821bed5efe
Lucymarine <96936875+Lucymarine@users.noreply.github.com>
2022-02-25 21:03:06 +0300

    Add project files.

---
+++ DataAccessLayer/Abstrack/IBlogDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstrack
+{
+    public interface IBlogDal
+    {
+        List<Blog> ListAllCategory();
+        void AddBlog(Blog blog);
+        void DeleteBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetById(int id);
+    }
+}
